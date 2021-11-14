4598a1147b1e4440730e9ec174201384e67a73cd
user0481 <egisxxegis@gmail.com>
2021-11-14 23:35:37 +0200

    add tests for correct answers

--- Test.py
+++ Test.py
@@ -42,3 +42,46 @@ if __name__ == '__main__':
         the_options = ["a", "\nb", "\nc", "\nd", "\ne", '\nf']
         the_answers_out = extract_answers(the_answers_in, the_options, splitter_suffix='. ', end_splitting=False)
         error = pretty_print_yes_no("Extract case a. x b. x", the_answers_real == the_answers_out)
+
+    if not error:
+        the_indexes_in = [3, 2, 0, 1]
+        the_indexes_real = [2]
+        the_indexes_out = get_correct_indexes(the_indexes_in, '3')
+        error = pretty_print_yes_no("Single answer get correct", the_indexes_real == the_indexes_out)
+
+    if not error:
+        the_indexes_in = [3, 2, 0, 1]
+        the_indexes_real = [3, 4]
+        the_indexes_out = get_correct_indexes(the_indexes_in, '1,2')
+        error = pretty_print_yes_no("Multi answer get correct", the_indexes_real == the_indexes_out)
+
+    if not error:
+        the_answer_in = "2 1 3"
+        the_answer_real = True
+        the_answer_out = is_answer_correct(the_answer_in, [3, 1, 2])
+        error = pretty_print_yes_no("Multi answer is correct", the_answer_real == the_answer_out)
+
+    if not error:
+        the_answer_in = "2,"
+        the_answer_real = True
+        the_answer_out = is_answer_correct(the_answer_in, [2])
+        error = pretty_print_yes_no("Single answer is correct", the_answer_real == the_answer_out)
+
+    if not error:
+        the_answer_in = "2 1 4"
+        the_answer_real = False
+        the_answer_out = is_answer_correct(the_answer_in, [3, 1, 2])
+        error = pretty_print_yes_no("Multi answer is incorrect", the_answer_real == the_answer_out)
+
+    if not error:
+        the_answer_in = "1"
+        the_answer_real = False
+        the_answer_out = is_answer_correct(the_answer_in, [3, 1, 2])
+        error = pretty_print_yes_no("Multi answer is incorrect", the_answer_real == the_answer_out)
+
+    if not error:
+        the_answer_in = "2"
+        the_answer_real = False
+        the_answer_out = is_answer_correct(the_answer_in, [1])
+        error = pretty_print_yes_no("Single answer is incorrect", the_answer_real == the_answer_out)
+
