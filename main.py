7f0affdf5872ff8da6fad0706751c84f55aadd06
user0481 <egisxxegis@gmail.com>
2022-01-04 16:50:10 +0200

    a little help message

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
+def print_hi():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Quizer.py - perform quiz (it questions you, you answer)')
+    print(f'Creator.py - make new questions')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
